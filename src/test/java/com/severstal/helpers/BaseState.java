ff40d9df63b771ef6a9b2501be4d6427b10c6563
da.vasilyev <dmitry.vasilyew@gmail.com>
2022-02-08 22:30:33 +0300

    added helpers

---
+++ src/test/java/com/severstal/helpers/BaseState.java
@@ -0,0 +1,41 @@
+package com.severstal.helpers;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseState {
+
+    @BeforeAll
+    public static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
+
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://severstal.com";
+        Configuration.browser = System.getProperty("browser");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = cfg.hostname();
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        Attachments.screenshotAs("Last screenshot");
+        Attachments.pageSource();
+        Attachments.browserConsoleLogs();
+        Attachments.addVideo();
+        closeWebDriver();
+    }
+
+}
